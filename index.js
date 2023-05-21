04dc0cb2b6312630ff587da4fdb98e3f5cbc0d5c
Subhankar-Chakrborty-au52 <subhankarchakrborty@gmail.com>
2023-05-21 13:55:44 +0530

    nothing

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const users = require("./models/users");
 const products = require("./models/products");
 const orders = require("./models/orders");
 
-const { hashPassword, comparePassword } = require("./utils");
-
 app.get("/test", async (req, res) => {
   const data = await users.find({});
 
@@ -27,85 +25,47 @@ app.get("/test", async (req, res) => {
 
 // Users APIs
 app.post("/user/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const userData = await users.findOne({
-      email,
-    });
 
-    if (!userData) {
-      throw { message: "Email does not exist!", statusCode: 404 };
-    }
+const  {firstName,lastName, email, password } = req.body;
+// console.log('firstName,lastName, email, password ', firstName,lastName, email, password )
 
-    const dbPassword = userData.password;
+try{
+  const result = await users.create({ firstName,lastName, email, password });
+  res.send({status: 'success', msg : 'user login successfully'});
 
-    const isPasswordMatched = comparePassword(password, dbPassword);
+}catch (error){
+  res.status(201).send({status: 'error',msg: 'error indicate while login'})
 
-    if (!isPasswordMatched) {
-      throw { message: "Password does not matched!", statusCode: 401 };
-    }
-
-    const data = JSON.parse(JSON.stringify(userData));
-    delete data.password;
-
-    res.send({ data, message: "User login successfully", status: true });
-  } catch (error) {
-    const message = error?.message || "Error while login";
-    const statusCode = error?.statusCode || 400;
-    res.status(statusCode).send({ message, status: false });
 }
 });
 
 
 app.post("/user/sign-up", async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
-  try {
-    const userId = uuidv4();
-    const isAlreadyRegistered = await users.findOne({ email });
-    if (isAlreadyRegistered) {
-      throw { message: "Email already exist!" };
-    }
 
-    const result = await users.create({
-      firstName,
-      lastName,
-      email,
-      password: hashPassword(password),
-      userId,
-    });
-    res.send({ message: "User successfully signed-up!", status: true });
-  } catch (error) {
-    const message = error?.message || "Error while sign-up";
-    const statusCode = error?.statusCode || 400;
-    res.status(statusCode).send({ message, status: false });
+  const  {firstName,lastName, email, password } = req.body;
+try{
+   const result = await users.create({ firstName,lastName, email, password });
+  res.send({status: 'success', msg : 'user signed-up successfully'});
+
+}catch (error){
+  res.status(201).send({status: 'error',msg: 'error indicate while sign-up'})
 }
 });
 
+
 // Orders APIs
-app.get("/order/:id", async (req, res) => {
-  const { orderId, userId } = req.params;
-  try {
-    const data = await orders.findById(userId);
-    console.log(data);
-    res.send({ status: "success", msg: data });
-  } catch (error) {
-    res.status(400).send({ status: "error", msg: "error getting the id" });
-  }
-});
+app.get("/order/:id", async (req, res) => {});
+
+
+
+
 
 app.get("/order/list/:limit/:offset", async (req, res) => {});
 
-app.post("/order/create", async (req, res) => {
-  const user_order = req.body;
-  try {
-    const order_data = await orders.create(user_order);
-    res.send({ status: "success", msg: "order added successfully" });
-  } catch (error) {
-    res
-      .status(400)
-      .send({ status: "error", msg: "error while adding the order" });
-  }
-});
+
+
+app.post("/order/create", async (req, res) => {});
+
 
 // Products APIs
 app.get("/product/:id", async (req, res) => {});
