87b1f49fde09f4a4e9e2c1a350cb8440a52cf266
varun <haldharvarun@gmail.com>
2023-05-29 00:18:07 +0530

    basic functionality added

--- controllers/menu.js
+++ controllers/menu.js
@@ -5,145 +5,169 @@ const users = require("../models/users");
 const products = require("../models/products");
 const orders = require("../models/orders");
 
-
 // user login
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-      // console.log(typeof firstName)
-      const userData = await users.findOne({
-        email
-      });
-  
-      if (!userData) {
-        throw { message: "Email does not exist!", statusCode: 404 };
-      }
-  
-      const dbPassword = userData.password;
-  
-      const isPasswordMatched = comparePassword(password, dbPassword);
-  
-      if (!isPasswordMatched) {
-        throw { message: "Password does not matched!", statusCode: 401 };
-      }
-  
-      const data = JSON.parse(JSON.stringify(userData));
-      delete data.password;
-  
-      res.send({ data, message: "User login successfully", status: true });
-    } catch (error) {
-      const message = error?.message || "Error while login";
-      const statusCode = error?.statusCode || 400;
-      res.status(statusCode).send({ message, status: false });
+  const { email, password } = req.body;
+  try {
+    // console.log(typeof firstName)
+    const userData = await users.findOne({
+      email,
+    });
+
+    if (!userData) {
+      throw { message: "Email does not exist!", statusCode: 404 };
     }
+
+    const dbPassword = userData.password;
+
+    const isPasswordMatched = comparePassword(password, dbPassword);
+
+    if (!isPasswordMatched) {
+      throw { message: "Password does not matched!", statusCode: 401 };
+    }
+
+    const data = JSON.parse(JSON.stringify(userData));
+    delete data.password;
+
+    res.send({ data, message: "User login successfully", status: true });
+  } catch (error) {
+    const message = error?.message || "Error while login";
+    const statusCode = error?.statusCode || 400;
+    res.status(statusCode).send({ message, status: false });
   }
+};
 
 // user signup
 const signup = async (req, res) => {
-    const { firstName, lastName, email, password } = req.body;
-    try {
-      // console.log(typeof firstName)
-      const userId = uuidv4();
-      const isAlreadyRegistered = await users.findOne({ email });
-      if (isAlreadyRegistered) {
-        throw { message: "Email already exist!" };
-      }
-  
-      const result = await users.create({
-        firstName,
-        lastName,
-        email,
-        password: hashPassword(password),
-        userId,
-      });
-      res.send({ message: "User successfully signed-up!", status: true });
-    } catch (error) {
-      const message = error?.message || "Error while sign-up";
-      const statusCode = error?.statusCode || 400;
-      res.status(statusCode).send({ message, status: false });
+  const { firstName, lastName, email, password } = req.body;
+  try {
+    // console.log(typeof firstName)
+    const userId = uuidv4();
+    const isAlreadyRegistered = await users.findOne({ email });
+    if (isAlreadyRegistered) {
+      throw { message: "Email already exist!" };
     }
+
+    const result = await users.create({
+      firstName,
+      lastName,
+      email,
+      password: hashPassword(password),
+      userId,
+    });
+    res.send({ message: "User successfully signed-up!", status: true });
+  } catch (error) {
+    const message = error?.message || "Error while sign-up";
+    const statusCode = error?.statusCode || 400;
+    res.status(statusCode).send({ message, status: false });
   }
+};
 
 // creating order
 const ordercreate = async (req, res) => {
-    const user_order = req.body;
-    try {
-      const order_data = await orders.create(user_order);
-      res.send({ status: "success", msg: "order added successfully" });
-    } catch (error) {
-      res
-        .status(400)
-        .send({ status: "error", msg: "error while adding the order" });
-    }
+  const user_order = req.body;
+  try {
+    const order_data = await orders.create({
+      ...user_order,
+      orderId: uuidv4(),
+    });
+    res.send({ status: "success", msg: "order added successfully" });
+  } catch (error) {
+    res
+      .status(400)
+      .send({ status: "error", msg: "error while adding the order" });
   }
+};
 
 // order id
 const orderId = async (req, res) => {
-    const { orderId, userId } = req.params;
-    try {
-      const data = await orders.findOne({userId});
-      console.log(data);
-      res.send({ status: "success", msg: data });
-    } catch (error) {
-      res.status(400).send({ status: "error", msg: "error getting the id" });
-    }
+  const { orderId, userId } = req.params;
+  try {
+    const data = await orders.findOne({ userId });
+    console.log(data);
+    res.send({ status: "success", msg: data });
+  } catch (error) {
+    res.status(400).send({ status: "error", msg: "error getting the id" });
   }
+};
 
 // order list limit
 const orderlist = async (req, res) => {
-    let { limit , offset } = req.params;
-    offset = (offset - 1) * limit ;
-    try {
-      const order_data = await orders.find({}).skip(offset).limit(limit);
-      res.send({message: "Response", order_data });
-    } catch (error) {
-      res.send({status : 'error' , msg : error});
-    }
+  let { limit, offset, userId } = req.params;
+  offset = (offset - 1) * limit;
+  try {
+    const data = await orders.find({ userId }).skip(offset).limit(limit);
+    res.send({ message: "Response", data });
+  } catch (error) {
+    res.send({ status: "error", msg: error });
   }
+};
 
 // product create
 const productcreate = async (req, res) => {
-    const product_details = req.body;
-    try {
-      const product_data = await products.create(product_details);
-      res.send({status : success , msg : 'Product added successfully' , product_data});
-    } catch (error) {
-      res.send({status : 'error' , msg : error});
-    }
+  const {
+    productImage,
+    productName,
+    productPrice,
+    productDescription,
+    totalItem,
+    userId,
+  } = req.body;
+
+  const productData = {
+    productImage,
+    productName,
+    productPrice,
+    productDescription,
+    totalItem,
+    userId,
+    productId: uuidv4(),
+  };
+
+  try {
+    const data = await products.create(productData);
+    res.send({
+      status: success,
+      msg: "Product added successfully",
+      data,
+    });
+  } catch (error) {
+    res.send({ status: "error", msg: error });
   }
+};
 
 // product id
 const productId = async (req, res) => {
-    const { productId } = req.params;
-    try {
-      const product_dataId = await products.findOne({productId});
-      res.send({status : 'success' , product_dataId});
-    } catch (error) {
-      res.send({status : 'error' , msg : 'error finding the product' , error});
-    }
+  const { productId } = req.params;
+  try {
+    const data = await products.findOne({ productId });
+    res.send({ status: "success", data });
+  } catch (error) {
+    res.send({ status: "error", msg: "error finding the product", error });
   }
+};
 
 // product list limit
 const productlist = async (req, res) => {
-    let { limit, offset } = req.params;
-    offset = (offset - 1) * limit;
-  
-    const data = await products.find({}).skip(offset).limit(limit);
-  
-    return res.send({
-      message: "Response",
-      data,
-    });
-  }
+  let { limit, offset } = req.params;
+  offset = (offset - 1) * limit;
+
+  const data = await products.find({}).skip(offset).limit(limit);
+
+  return res.send({
+    message: "Response",
+    data,
+  });
+};
 
 // exports
 module.exports = {
-    login,
-    signup,
-    ordercreate,
-    orderId,
-    orderlist,
-    productcreate,
-    productId,
-    productlist
-}
+  login,
+  signup,
+  ordercreate,
+  orderId,
+  orderlist,
+  productcreate,
+  productId,
+  productlist,
+};
--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+const localStorageUserKey = "food_app_login_data";
 
 // signin
 async function onClickLogin() {
@@ -10,11 +11,18 @@ async function onClickLogin() {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
-  })
+  });
   // .then((response) => response.json());
   const jsonResponse = await response.json();
+  console.log("jsondata", jsonResponse);
 
   if (jsonResponse?.status) {
+    const userId = jsonResponse?.data?.userId || "";
+
+    if (userId) {
+      localStorage.setItem(localStorageUserKey, userId);
+    }
+
     window.location = "/index.html";
   } else {
     const errorMessage = jsonResponse?.message;
@@ -23,7 +31,6 @@ async function onClickLogin() {
     errorTag.style.display = "inline";
   }
 }
-
 
 // signup
 async function onclicksignup() {
@@ -38,12 +45,11 @@ async function onclicksignup() {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ firstname , lastname , email, password  }),
+    body: JSON.stringify({ firstname, lastname, email, password }),
   });
-  const jsondata = response.json();
-
+  const jsondata = await response.json();
   // .then((response) => response.json());
-  if(password !== confirmPassword){
+  if (password !== confirmPassword) {
     const errorMessage = response?.message;
     const errorTag = document.getElementById("password-error");
     errorTag.innerText = errorMessage;
@@ -51,7 +57,7 @@ async function onclicksignup() {
   }
 
   if (jsondata?.status) {
-    window.location = "/index.html";
+    window.location = "/signin.html";
   } else {
     const errorMessage = jsondata?.message;
     const errorTag = document.getElementById("signup-error");
@@ -59,7 +65,6 @@ async function onclicksignup() {
     errorTag.style.display = "inline";
   }
 }
-
 
 // products
 async function showProducts() {
@@ -73,31 +78,163 @@ async function showProducts() {
   if (response?.data?.length) {
     let htmlTags = "";
     for (let index = 0; index < response?.data?.length; index++) {
-      const { productDescription, productImage, productName, productPrice } =
-        response?.data[index];
+      const {
+        productDescription,
+        productImage,
+        productName,
+        productPrice,
+        productId,
+      } = response?.data[index];
       htmlTags = `${htmlTags} 
         <div class="card" style="width: 18rem;">
         <img src=${productImage[0]} class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${productName}y</h5>
             <p class="card-text">${productDescription}</p>
+            <h5 class="card-text">${productPrice}</h5>
+            <a href="#" class="btn btn-success" onclick="createOrder('${productId}', ${productPrice})" id="buy">Buy</a>
         </div>
     </div>
         `;
     }
-
-    // const parentDiv = document.getElementById("cards");
-    // parentDiv.innerHTML = htmlTags;
+    const parentDiv = document.getElementById("menu-cards");
+    parentDiv.innerHTML = htmlTags;
   }
 }
 
-// append menu product cards to my orders
-const buy = document.getElementById('buy');
-buy.addEventListener('click' , ()=> {
-  
-})
+async function createOrder(productId, orderPrice) {
+  const userId = getUserIdFromLocalStorage();
+  if (userId) {
+    const fields = {
+      totalItem: 1,
+      userId,
+      productId,
+      orderPrice,
+    };
+    const response = await fetch("/order/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(fields),
+    });
 
+    const jsondata = await response.json();
+
+    if (jsondata?.status) {
+      window.location = "/myorder.html";
+    } else {
+      // const errorMessage = jsondata?.message;
+      // const errorTag = document.getElementById("signup-error");
+      // errorTag.innerText = errorMessage;
+      // errorTag.style.display = "inline";
+    }
+  }
+}
+
+// order
+async function showMyOrder(userId) {
+  const response = await fetch(`/order/list/${userId}/10/1`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then(async (response) => await response.json());
+
+  if (response?.data?.length) {
+    let htmlTags = "";
+    for (let index = 0; index < response?.data?.length; index++) {
+      const { orderPrice, totalItem, productId } = response?.data[index];
+
+      const res = await fetch(`/product/${productId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then(async (res) => await res.json());
+
+      console.log("response", res);
+
+      const productName = res?.data?.productName || "nnn";
+
+      htmlTags = `${htmlTags} 
+      <div class="list_details">
+      <h2>${productName}</h2>
+      <h2>${totalItem}</h2>
+      <h2>${orderPrice}</h2>
+  </div>
+        `;
+    }
+    const parentDiv = document.getElementById("my-orders");
+    parentDiv.innerHTML = htmlTags;
+  }
+}
+
+const getUserIdFromLocalStorage = () => {
+  return localStorage.getItem(localStorageUserKey);
+};
+
+const logout = () => {
+  localStorage.removeItem(localStorageUserKey);
+  window.location = "/signin.html";
+};
+
+async function addProduct() {
+  const userId = getUserIdFromLocalStorage();
+  if (userId) {
+    const fields = {
+      totalItem: 10,
+      userId,
+    };
+    const products = document.getElementsByClassName("addProduct");
+    for (let i = 0; i < products.length; i++) {
+      const name = products[i].name;
+      const value = products[i].value;
+      if (name === "productImage") {
+        fields[name] = value.split(",");
+      } else {
+        fields[name] = value;
+      }
+    }
+    const response = await fetch("/product/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(fields),
+    });
+
+    const jsondata = await response.json();
+
+    if (jsondata?.status) {
+      window.location = "/menu.html";
+    } else {
+      // const errorMessage = jsondata?.message;
+      // const errorTag = document.getElementById("signup-error");
+      // errorTag.innerText = errorMessage;
+      // errorTag.style.display = "inline";
+    }
+  }
+}
+
+// // append menu product cards to my orders
+// const buy = document.getElementById("buy");
+// buy.addEventListener("click", () => {});
 
 function homeOnLoad() {
+  const userId = getUserIdFromLocalStorage();
+  if (userId) {
+    document.getElementById("user-section").style.display = "none";
+    document.getElementById("user-section-logout").style.display = "inline";
+  }
   showProducts();
+}
+
+function homeLoadMyOrder() {
+  const userId = getUserIdFromLocalStorage();
+  if (userId) {
+    document.getElementById("user-section").style.display = "none";
+    document.getElementById("user-section-logout").style.display = "inline";
+  }
+  showMyOrder(userId);
 }
